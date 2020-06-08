4f8b2356397eea95f8ba28ca982f43cbd9e4e58c
Mark Diekhans <markd@ucsc.edu>
2020-06-08 11:10:14 -0700

    work on repeatmasker

---
+++ build/t2tChm13_20200602/setup.sh
@@ -0,0 +1,9 @@
+export t2t_asmver=t2tChm13_20200602
+
+export t2t_endpoint=9db1f0a6-a05a-11ea-8f06-0a21f750d19b
+export my_endpoint=13298548-a76c-11ea-8f09-0a21f750d19b
+export t2t_rootdir=/cluster/home/markd/compbio/nanopore/projs/t2t-chm13
+export t2t_builddir=${t2t_rootdir}/build/${t2t_asmver}
+export t2t_srcdir=${t2t_rootdir}/T2T-CHM13-hub/
+export t2t_bindir=${t2t_rootdir}/T2T-CHM13-hub/bin
+export PATH=${t2t_bindir}:${PATH}
